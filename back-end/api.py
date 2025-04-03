482e3baa9e98c601e41a9cf3f2f211275f125af1
Caio Santana <caiohenriq98@gmail.com>
2025-04-02 21:09:09 -0300

    Criação da classe de commits

--- back-end/api.py
+++ back-end/api.py
@@ -14,12 +14,26 @@ class APIGit:
         self.name = dados.get('name', '')
         self.id = dados.get('id', '')
 
-
-    def __repr__(self):
+        def __repr__(self):
         return f"Login: {self.login}, Name: {self.name}, ID: {self.id}"
+
     
+class Commits:
+    def __init__(self, login, repos):
+        self.login = login
+        self.repos = repos
+
+        self.get_commits()
+    def get_commits(self):
+        resposta = requests.get(f"https://api.github.com/repos/{self.login}/{self.repos}/commits")
+        dados = resposta.json()
+        return {'commits': len(dados)}
+
+
+
+
 
 
 if __name__ == '__main__':
-    meu_git = APIGit("freitasthiiago")
+    meu_git = Commits("lucasmarkes", "next-exercicios")
     print(meu_git)
